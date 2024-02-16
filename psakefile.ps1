f23dfd941a2da01e82d73d5f90e3683492dfbbb5
Daniel Zollinger <krdmgwngb8@snkmail.com>
2024-02-16 16:37:09 +1300

    build: refactor compile tasks
    Move lists of diagram files up into properties, and create a common
    `Compile-PumlDiagram` function that all the compile tasks can use, the
    net result is that very little is left in the tasks themselves, which
    hopefully makes them easier to read.

--- psakefile.ps1
+++ psakefile.ps1
@@ -5,6 +5,29 @@ Properties {
   $SrcRoot = (Join-Path $PSScriptRoot "src")
   [string[]] $OutputFormats = ,"svg"
 
+  # PlantUML/Dot diagrams to compile
+  $DiagramFiles = @{
+    hc = (
+      "hc_2h_progression_1.puml",
+      "hc_2h_progression_2.puml",
+      "hc_2h_progression_ALL.puml",
+      "hc_2h_progression_ALT.puml",
+      "hc_sa_progression.puml"
+    )
+    kb = (
+      "kb_progression_1.puml",
+      "kb_progression_2_tgu.puml",
+      "kb_progression_3.puml",
+      "kb_progression_4.puml",
+      "kb_progression_ALL.puml"
+    )
+    mace = (
+      "mace_progression_ALL.puml",
+      "mace_progression_sa.puml",
+      "mace_warrior_subsequence.puml"
+    )
+  }
+
   # Files copied directly to Dist
   $StaticFiles = @{
     hc = (
@@ -25,65 +48,46 @@ Task default -Depends Full
 Task Full -Depends Clean, Compile, CopyStaticFiles {
 }
 
-Task Compile -Depends CompileHeavyClub, CompileKettlebell, CompileMace {
-}
+Task Compile -Depends CompileHeavyClub, `
+                      CompileKettlebell, `
+                      CompileMace
+{}
 
-Task CompileHeavyClub {
-  $HCRoot = (Join-Path $SrcRoot "hc")
-  
-  $PumlFiles = "hc_2h_progression_1.puml",
-               "hc_2h_progression_2.puml",
-               "hc_2h_progression_ALL.puml",
-               "hc_2h_progression_ALT.puml",
-               "hc_sa_progression.puml"
-  foreach ($SrcFile in $PumlFiles) {
-    Write-Host "Rendering $SrcFile"
-    Puml -SrcFilePath (Join-Path $HCRoot $SrcFile) `
-         -OutputDirPath $DistDir `
-         -OutputFormatList $OutputFormats
-  }
-}
+Task CompileHeavyClub { Compile-PumlDiagram -ToolName "hc" }
 
-Task CompileKettlebell {
-  $KBRoot = (Join-Path $SrcRoot "kb")
-  $PumlFiles = "kb_progression_1.puml",
-               "kb_progression_2_tgu.puml",
-               "kb_progression_3.puml",
-               "kb_progression_4.puml",
-               "kb_progression_ALL.puml"
-  foreach ($SrcFile in $PumlFiles) {
-    Write-Host "Rendering $SrcFile"
-    Puml -SrcFilePath (Join-Path $KBRoot $SrcFile) `
-          -OutputDirPath $DistDir `
-          -OutputFormatList $OutputFormats
-  }
-}
+Task CompileKettlebell { Compile-PumlDiagram -ToolName "kb" }
 
-Task CompileMace {
-  $MaceRoot = (Join-Path $SrcRoot "mace")
-  $PumlFiles = "mace_progression_ALL.puml",
-               "mace_progression_sa.puml",
-               "mace_warrior_subsequence.puml"
-  foreach ($SrcFile in $PumlFiles) {
-    Write-Host "Rendering $SrcFile"
-    Puml -SrcFilePath (Join-Path $MaceRoot $SrcFile) `
-        -OutputDirPath $DistDir `
-        -OutputFormatList $OutputFormats
-  }
-}
+Task CompileMace { Compile-PumlDiagram -ToolName "mace" }
 
 Task CopyStaticFiles -Depends CopyStaticHeavyClub, `
                               CopyStaticKettlebell, `
                               CopyStaticMace
 {}
 
-Task CopyStaticHeavyClub { Copy-Static -ToolName "hc" }
+Task CopyStaticHeavyClub { Copy-StaticFiles -ToolName "hc" }
 
-Task CopyStaticKettlebell { Copy-Static -ToolName "kb" }
+Task CopyStaticKettlebell { Copy-StaticFiles -ToolName "kb" }
 
-Task CopyStaticMace { Copy-Static -ToolName "mace" }
+Task CopyStaticMace { Copy-StaticFiles -ToolName "mace" }
 
-function Copy-Static {
+Task Clean {
+  Remove-Item -Path (Join-Path $DistDir "*")
+}
+
+function Compile-PumlDiagram {
+  param (
+    [string] $ToolName
+  )
+  $ToolRoot = (Join-Path $SrcRoot $ToolName)
+  foreach ($SrcFile in $DiagramFiles[$ToolName]) {
+    Write-Host "Rendering $SrcFile"
+    Puml -SrcFilePath (Join-Path $ToolRoot $SrcFile) `
+        -OutputDirPath $DistDir `
+        -OutputFormatList $OutputFormats
+  }
+}
+
+function Copy-StaticFiles {
   param (
     [string] $ToolName
   )
@@ -95,6 +99,4 @@ function Copy-Static {
   }
 }
 
-Task Clean {
-  Remove-Item -Path (Join-Path $DistDir "*")
-}
+
